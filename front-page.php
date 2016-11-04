620411ee359ed16ba196c42679927dfbd430f64a
Lauren Greenway <lagreenway@gmail.com>
2016-11-04 14:42:34 -0400

    friday afternoon

--- front-page.php
+++ front-page.php
@@ -1,53 +1,40 @@
 <?php get_header();  ?>
 
-<div class="hero">
-  <div class="container">
-    <?php // WP_Query arguments
-      $args = array (
-        'nopaging'               => false,
-        'posts_per_page'         => '5',
-        'ignore_sticky_posts'    => true,
-        'offset'                 => '0',
-      );
-
-      // The Query
-      $homePagePosts = new WP_Query( $args );
-
-      // The Loop
-      if ( $homePagePosts->have_posts() ) {
-        while ( $homePagePosts->have_posts() ) {
-          $homePagePosts->the_post();
-          // do something
-          '<h1>' . the_title() . '</h1>';
-          the_content();
-        }
-      } else {
-        // no posts found
-      }
-
-      // Restore original Post Data
-      wp_reset_postdata(); ?>
-  </div>
-
-</div> 
-
 <div class="main">
   <div class="container">
-
-    <div class="content">
-      <?php the_title(); ?>
-      <?php the_field('home_latest_stories'); ?>
-      <?php the_field('home_description'); ?>
-      <?php the_field('home_view_more') ?>
-
-      <?php // Start the loop ?>
-      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-        
-
-      <?php endwhile; // end the loop?>
-    </div> <!-- /,content -->
-
-    
+    <section class="latest-posts content grid">
+        <div class="grid-cell">
+          <h2><?php the_field('home_latest_stories'); ?></h2>
+          <p><?php the_field('home_description'); ?></p>
+          <button class="btn"></button><?php the_field('home_view_more') ?>
+        </div>
+      <?php 
+        $postLoop = new WP_Query(array(
+          'post_type' => 'post',
+          'offset' => 1,
+          'posts_per_page' => 3
+        ));
+        if($postLoop->have_posts()) while($postLoop->have_posts()): $postLoop->the_post()
+      ?>
+        <div class="grid-cell">
+        <?php  ?>
+          <p><?php the_author(); ?></p>
+          <p><?php the_time('F j, Y'); ?></p>
+          <p><?php the_tags(); ?></p>
+          <?php the_title() ?> 
+          <?php the_post_thumbnail('medium') ?>
+        </div>
+    <?php endwhile;?>
+    <?php wp_reset_postdata(); ?>
+    </section> <!-- /,content -->
+    <section class="quote content">
+      <h4><?php the_field('home_quote_tagline'); ?></h4>
+      <h2><?php the_field('home_quote'); ?></h2>
+      <p><?php the_field('home_quote_explained'); ?></p>
+    </section>
+    <section class="insta">
+      <?php echo wdi_feed(array('id'=>'1')); ?>
+    </section>
 
   </div> <!-- /.container -->
 </div> <!-- /.main -->
--- header.php
+++ header.php
@@ -17,26 +17,79 @@
 <body <?php body_class(); ?> >
 
 <header>
-    <?php wp_nav_menu( array(
-      'container' => false,
-      'theme_location' => 'social'
-    )); ?>
-    <h1>
-      <a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo( 'name', 'display' ); ?>" rel="home">
-        <?php bloginfo( 'name' ); ?>
-      </a>
-    </h1>
+ 
+    <?php if ( is_front_page() ) { ?>
+    <nav class="home grid">
+      <div class="container grid-cell">
+        <?php wp_nav_menu( array(
+          'container' => false,
+          'theme_location' => 'social'
+        )); ?>
+        <h1>
+          <a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo( 'name', 'display' ); ?>" rel="home">
+            <?php bloginfo( 'name' ); ?>
+          </a>
+        </h1>
 
-    <?php wp_nav_menu( array(
-      'container' => false,
-      'theme_location' => 'primary'
-    )); ?>
+        <?php wp_nav_menu( array(
+          'container' => false,
+          'theme_location' => 'primary'
+        )); ?>
+      </div>
+    </nav>
 
-    <?php if ( is_front_page() ) {    
-    echo'<h2>Only Displays On The Front Page</h2>';
+        <?php // WP_Query arguments
+          $args = array (
+            'nopaging'               => false,
+            'posts_per_page'         => '1',
+            'ignore_sticky_posts'    => true,
+            'offset'                 => '0',
+          );
+
+          // The Query
+          $homePagePosts = new WP_Query( $args );
+
+          // The Loop
+          if ( $homePagePosts->have_posts() ) {
+            while ( $homePagePosts->have_posts() ) {
+
+              $homePagePosts->the_post();
+              ?>
+              <div class="hero grid" style="background-image: url(<?php echo the_post_thumbnail_url('large'); ?>)">
+                <div class="hero-container">
+                  <h1><?php  the_title() ?></h1>
+                  <?php $content = get_the_content(); ?>
+                  <p><?php echo wp_trim_words($content, 20); ?></p>
+                  <a href="<?php echo get_post_permalink(); ?>"><button class="btn">View More</button></a>
+              <?
+            }
+          } else {
+            // no posts found
+          }
+
+          // Restore original Post Data
+          wp_reset_postdata(); ?>
+      </div>
+
+    </div> 
+    <?php
     }
-    else {
-      echo '<h2>Not the front page</h2>';
+    else { ?>
+      <nav class="main">
+        <div class="container grid">
+          <h1>
+            <a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo( 'name', 'display' ); ?>" rel="home">
+              <?php bloginfo( 'name' ); ?>
+            </a>
+          </h1>
+
+          <?php wp_nav_menu( array(
+            'container' => false,
+            'theme_location' => 'primary'
+          )); ?>
+        </div>
+      </nav>
+    <?php
     }
  ?>
 </header><!--/.header-->
